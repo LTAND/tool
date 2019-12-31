57c41afe56c9db36647acb929924c2d6bbc9de3a
LTAND <horrizion@foxmail.com>
2020-01-01 00:06:53 +0800

    init

---
+++ utils/jsonp.js
@@ -0,0 +1,39 @@
+import originJsonp from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+export function param(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += '&' + k + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : ''
+}
+// export function getSongList(disstid) {
+//   const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+
+//   const data = Object.assign({}, commonParams, {
+//     disstid,
+//     type: 1,
+//     json: 1,
+//     utf8: 1,
+//     onlysong: 0,
+//     platform: 'yqq',
+//     hostUin: 0,
+//     needNewCode: 0
+//   })
+// return jsonp(url, data, {param: 'jsonpCallback'})
+// }
