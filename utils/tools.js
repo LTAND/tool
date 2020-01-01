ccb3fab2537ad85c518acfeaf391e0baadf2b191
LTAND <horrizion@foxmail.com>
2020-01-01 23:20:24 +0800

    add some tool

--- utils/tools.js
+++ utils/tools.js
@@ -77,10 +77,38 @@ const keyword = (str, value) => {
   return str;
 }
 
+
+/**
+ * 格式化日期 yyyy-mm-dd HH:MM
+ * 
+ * @param String format 
+ * @param Object date 
+ */
+function getDateFormat(format,date) {
+  var o = {
+  "M+": date.getMonth() + 1, //月份
+  "d+": date.getDate(), //日
+  "H+": date.getHours(), //小时
+  "m+": date.getMinutes(), //分
+  "s+": date.getSeconds(), //秒
+  "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+  "f+": date.getMilliseconds() //毫秒
+};
+if (/(y+)/.test(format))
+  format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+for (var k in o)
+  if (new RegExp("(" + k + ")").test(format))
+    format = format.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+return format;
+}
+
+
+
 export {
   addLocCacheTime, // 给本地缓存添加个时间
   getLocCacheDate, // 获取本地缓存数据
   throttle, // 函数节流
   chunk, // 数组分块
-  keyword // 搜索关键词高亮显示
+  keyword, // 搜索关键词高亮显示
+  getDateFormat //  格式化日期 getDateFormat("yyyy-mm-dd HH:MM", new Date())
 };
